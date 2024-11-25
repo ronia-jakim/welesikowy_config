2f9d8edb480346a45cdb72dd0d960354fad86b65
dupa <weronikajakimowicz2@gmail.com>
2024-11-25 10:14:40 +0100

    weles

--- scripts/change_gtk_theme.sh
+++ scripts/change_gtk_theme.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/bash
 
-let theme=$(~/welesikowy_config/scripts/light-or-dark.sh)
+theme=$(~/welesikowy_config/scripts/light-or-dark.sh)
+
+echo $theme
 
 if [[ $theme == *"light"* ]];
 then 
+  echo "light theme activated"
   gsettings set org.gnome.desktop.interface gtk-theme "Everforest-Light"
   gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
 else
+  echo "dark theme activated"
   gsettings set org.gnome.desktop.interface gtk-theme "Everforest-Dark"
   gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 fi
