8faff750409e394675f36fbf554a489cefda3813
ronia-jakim <weronikajakimowicz2@gmail.com>
2025-07-06 12:58:19 +0200

    whichkey-delay

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -14,7 +14,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-
 require "settings"
 require "mappings"
 
--- nvim/.config/nvim/lua/plugins/whichkey.lua
+++ nvim/.config/nvim/lua/plugins/whichkey.lua
@@ -1,13 +1,8 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-
-  },
   config = function()
+    require('which-key').setup( { delay = 1000 } )
     require("which-key").add({
       {
         "<leader>?",
