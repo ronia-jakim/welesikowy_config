4c2ebf3ecdcd7b3ae9f538ffdb27bddeaf0a4b8e
ronia-jakim <weronikajakimowicz2@gmail.com>
2025-08-01 18:04:26 +0200

    oil

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,8 +1,10 @@
 return {
   'stevearc/oil.nvim',
   config = function()
-    require("oil").setup()
-    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    require("oil").setup({
+      default_file_explorer = false
+    })
+    -- vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
   end,
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
