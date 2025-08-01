dd164849100ca9c76d6fc132b4a06096bd3ba683
ronia-jakim <weronikajakimowicz2@gmail.com>
2025-08-01 17:58:20 +0200

    oil

---
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,11 @@
+return {
+  'stevearc/oil.nvim',
+  config = function()
+    require("oil").setup()
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+  end,
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  lazy = false,
+}
