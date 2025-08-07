ba7725d8b90a45236ed907785c7445d31061aeaf
ronia-jakim <weronikajakimowicz2@gmail.com>
2025-08-07 14:48:30 +0200

    tailwind

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -20,7 +20,7 @@ return {
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
     local lspconfig = require('lspconfig')
     -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-    local servers = { 'clangd', 'pyright', 'texlab', 'julials', 'lua_ls', 'csharp_ls', 'ts_ls', } -- 'ltex_plus' }
+    local servers = { 'clangd', 'pyright', 'texlab', 'julials', 'lua_ls', 'csharp_ls', 'ts_ls', 'tailwindcss' } -- 'ltex_plus' }
     for _, lsp in ipairs(servers) do
       lspconfig[lsp].setup {
         -- on_attach = my_custom_on_attach,
--- nvim/.config/nvim/lua/plugins/css.lua
+++ nvim/.config/nvim/lua/plugins/css.lua
@@ -1,6 +1,21 @@
+-- return {
+--   'brenoprata10/nvim-highlight-colors',
+--   config = function()
+--     require('nvim-highlight-colors').setup({})
+--   end
+-- }
+
 return {
-  'brenoprata10/nvim-highlight-colors',
-  config = function()
-    require('nvim-highlight-colors').setup({})
-  end
+    "catgoose/nvim-colorizer.lua",
+    event = "BufReadPre",
+    opts = {
+      user_default_options = {
+        mode = 'virtualtext', 
+        css = true, 
+        tailwind = 'both',
+        tailwind_opts = {
+          update_names = true
+        }
+      }
+    },
 }
