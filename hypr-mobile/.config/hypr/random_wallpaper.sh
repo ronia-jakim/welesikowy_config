63771cea1d9b33e61ef173aa969a9230a7bcb809
ronia-jakim <weronikajakimowicz2@gmail.com>
2025-09-08 16:54:31 +0200

    wpaperd

--- hypr-mobile/.config/hypr/random_wallpaper.sh
+++ hypr-mobile/.config/hypr/random_wallpaper.sh
@@ -1,8 +1,36 @@
-while true
-do
-  wallpaper=$(ls ~/Pictures/wallpapers | shuf -n 1)
+WALLPAPER_DIR="$HOME/Pictures/wallpapers"
 
-  swaybg -i ~/Pictures/wallpapers/$wallpaper -m fill
-  
-  sleep 10m
+
+for file in "$WALLPAPER_DIR"/*; do
+    # Only handle files (skip directories)
+    if [ -f "$file" ]; then
+        echo "$file"
+        hyprctl hyprpaper preload $file
+    fi
 done
+
+while true; do
+    # Wait 10 minutes
+    sleep 5
+    
+    # Pick a random wallpaper
+    wallpaper=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
+
+    # Apply it to every monitor
+    for monitor in $(hyprctl monitors -j | jq -r '.[].name'); do
+        hyprctl hyprpaper wallpaper "$monitor, $wallpaper"
+    done
+
+done
+
+
+# while true
+# do
+#   wallpaper=$(ls ~/Pictures/wallpapers | shuf -n 1)
+#
+#   swaybg -i ~/Pictures/wallpapers/$wallpaper -m fill
+#
+#   sleep 10s
+#
+#   pkill swaybg
+# done
